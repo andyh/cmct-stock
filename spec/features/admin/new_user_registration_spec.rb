bd651cdd3e2376513b20e5bd528794ccbed7d3e9
Andy Henson <andrew.henson@foxsoft.net>
2015-09-06 23:24:27 +0100

    Fix style violations

--- spec/features/admin/new_user_registration_spec.rb
+++ spec/features/admin/new_user_registration_spec.rb
@@ -1,13 +1,14 @@
 require "rails_helper"
 
-feature 'New user registration' do
-  scenario 'successful sign up' do
+feature "New user registration" do
+  scenario "successful sign up" do
     visit new_admin_user_path
 
-    fill_form(:user, { first_name: "Tony", last_name: "Stark", email: "tony@example.com", password: "password", role: "Stockholder", active: true })
+    fill_form(:user, { first_name: "Tony", last_name: "Stark",
+                       email: "tony@example.com", password: "password",
+                       role: "Stockholder", active: true })
     click_on submit(:user)
 
-    expect(page).to have_content t('user.create.success')
+    expect(page).to have_content t("user.create.success")
   end
 end
-
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,7 @@ require "rails_helper"
 RSpec.describe User do
   it { should validate_presence_of(:first_name) }
   it { should validate_presence_of(:last_name) }
-  it { should validate_inclusion_of(:role)
-         .in_array(%w{admin stockholder manager}) }
+  it { should validate_inclusion_of(:role).in_array(%w{admin stockholder manager}) }
 
   describe "#full_name" do
     it "returns the users full name" do
@@ -14,4 +13,3 @@ RSpec.describe User do
     end
   end
 end
-
