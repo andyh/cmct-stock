dda78224999087259c452b0425950b5e6e5aa2cf
Andy Henson <andrew.henson@foxsoft.net>
2015-09-06 15:58:43 +0100

    Add basic validations for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   include Clearance::User
+  VALID_ROLES = %w{stockholder manager admin}
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :role, inclusion: VALID_ROLES
 end
+
