54dd0cb3acbd9799f83858919fcc950962f130e2
Andy Henson <andrew.henson@foxsoft.net>
2015-09-13 14:57:16 +0100

    Add #has_role? method to User
    A simple way to check if the user has a specific role. Added a check here for
    invalid roles just as a simple guard against a role not being set before
    checking if the user has a given role as well as preventing a bad role being
    assigned and the user not having been validated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
+class InvalidRole < StandardError; end
+
 class User < ActiveRecord::Base
+
   include Clearance::User
   VALID_ROLES = %w{stockholder manager admin}
   validates :first_name, presence: true
@@ -7,6 +10,11 @@ class User < ActiveRecord::Base
 
   def full_name
     [first_name, last_name].join(" ")
+  end
+
+  def has_role?(role_to_check)
+    raise InvalidRole.new("Role does not exist") unless role_to_check.to_s.in?(VALID_ROLES)
+    role_to_check.to_s == role
   end
 end
 
