61cc9d0bf3c0a89a36e55718ba83fd13042c0e5d
Andy Henson <andrew.henson@foxsoft.net>
2015-09-22 09:31:23 +0100

    Make dashboard accurately reflect users stock figures

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,8 @@ class DashboardController < AuthenticatedController
 
   def index
     authorize :dashboard, :show?
+    @stock_amount = current_user.stock.map do |item|
+      item.product.price * item.quantity
+    end.sum
   end
 end
